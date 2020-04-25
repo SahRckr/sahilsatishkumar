fe0f7edc2ebf521ba2078c74351b9648bfe43a1d
Sahil Satishkumar <sahilsatishkumar@gmail.com>
2020-04-25 20:03:31 +0530

    inital changes completed

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,97 +1,113 @@
-import React from 'react';
-import classnames from 'classnames';
-import Layout from '@theme/Layout';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import useBaseUrl from '@docusaurus/useBaseUrl';
-import styles from './styles.module.css';
+import React from "react";
+import classnames from "classnames";
+import Layout from "@theme/Layout";
+import Link from "@docusaurus/Link";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import useBaseUrl from "@docusaurus/useBaseUrl";
+import styles from "./styles.module.css";
 
-const features = [
-  {
-    title: <>Easy to Use</>,
-    imageUrl: 'img/undraw_docusaurus_mountain.svg',
-    description: (
-      <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
-      </>
-    ),
-  },
-  {
-    title: <>Focus on What Matters</>,
-    imageUrl: 'img/undraw_docusaurus_tree.svg',
-    description: (
-      <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
-      </>
-    ),
-  },
-  {
-    title: <>Powered by React</>,
-    imageUrl: 'img/undraw_docusaurus_react.svg',
-    description: (
-      <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
-      </>
-    ),
-  },
+const sections = [
+	{
+		title: "Experience",
+		id: "experience",
+		items: [
+			{
+				mainText: "Software Engineer",
+				duration: "August. 2018 - Present",
+				subText: "Hypertrack Technologies Private Limited",
+			},
+			{
+				mainText: "Full Stack Javascript Developer",
+				duration: "January. 2017 - June. 2018",
+				subText: "Dataphi Analytics Private Limited",
+			},
+			{
+				mainText: "Web developer Intern",
+				duration: "July. 2016 - October. 2016",
+				subText: "Print With Pi (Acquired by Printo India)",
+			},
+		],
+	},
+	{
+		title: "Education",
+		id: "education",
+		items: [
+			{
+				mainText: "Udacity Nanodegree",
+				duration: "Feb. 2020 - Present",
+				subText: "Cloud developer",
+			},
+			{
+				mainText: "P.A. College of Engineering",
+				duration: "August. 2012 - January. 2020",
+				subText: "Computer Science & Engineering",
+			},
+		],
+	},
+	{
+		title: "Skills",
+		id: "skills",
+		items: [
+			{
+				mainText: "Languages and Frameworks",
+				subText:
+					"Javascript (ES6), React/React-Native, Redux/Redux-Saga, GraphQL/Apollo-GraphQL",
+			},
+			{
+				mainText: "Other Interests",
+				subText:
+					"AWS Amplify/AppSync, Functional Programming (specially with JS), Clojure",
+			},
+		],
+	},
 ];
 
-function Feature({imageUrl, title, description}) {
-  const imgUrl = useBaseUrl(imageUrl);
-  return (
-    <div className={classnames('col col--4', styles.feature)}>
-      {imgUrl && (
-        <div className="text--center">
-          <img className={styles.featureImage} src={imgUrl} alt={title} />
-        </div>
-      )}
-      <h3>{title}</h3>
-      <p>{description}</p>
-    </div>
-  );
+function ListItemComponent({ mainText, subText, duration }) {
+	return (
+		<div className={styles.listContainer}>
+			<b>{mainText}</b>
+			<i>{duration}</i>
+			<p>{subText}</p>
+		</div>
+	);
+}
+
+function ListSectionComponent({ items }) {
+	return (
+		<ul className={styles.listSection}>
+			{items.map((item) => (
+				<li>
+					<ListItemComponent key={item.mainText} {...item} />
+				</li>
+			))}
+		</ul>
+	);
+}
+
+function Section(props) {
+	const { id, title, items } = props;
+	return (
+		<section className={styles.sectionGroup}>
+			<h3 className={styles.sectionHeader}>{title}</h3>
+			<div className={styles.sectionContent}>
+				<ListSectionComponent items={items} />
+			</div>
+		</section>
+	);
 }
 
 function Home() {
-  const context = useDocusaurusContext();
-  const {siteConfig = {}} = context;
-  return (
-    <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
-      <header className={classnames('hero hero--primary', styles.heroBanner)}>
-        <div className="container">
-          <h1 className="hero__title">{siteConfig.title}</h1>
-          <p className="hero__subtitle">{siteConfig.tagline}</p>
-          <div className={styles.buttons}>
-            <Link
-              className={classnames(
-                'button button--outline button--secondary button--lg',
-                styles.getStarted,
-              )}
-              to={useBaseUrl('docs/doc1')}>
-              Get Started
-            </Link>
-          </div>
-        </div>
-      </header>
-      <main>
-        {features && features.length && (
-          <section className={styles.features}>
-            <div className="container">
-              <div className="row">
-                {features.map((props, idx) => (
-                  <Feature key={idx} {...props} />
-                ))}
-              </div>
-            </div>
-          </section>
-        )}
-      </main>
-    </Layout>
-  );
+	const context = useDocusaurusContext();
+	const { siteConfig = {} } = context;
+	return (
+		<Layout>
+			<main className={styles.mainContainer}>
+				{sections.map((sectionItem) => (
+					<Section key={sectionItem.id} {...sectionItem} />
+				))}
+			</main>
+		</Layout>
+	);
 }
 
 export default Home;
