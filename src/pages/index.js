f4325f86ddd529c22ab03f43371ebc350c5825be
Sahil Satishkumar <sahilsatishkumar@gmail.com>
2020-08-21 21:55:18 +0530

    updated landing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,9 +5,31 @@ import Link from "@docusaurus/Link";
 import styles from "./styles.module.css";
 
 function Home() {
+	React.useEffect(() => {
+		const mainWrapper = document.querySelector(".main-wrapper");
+		mainWrapper.classList.add(styles.columnFlex);
+		return () => mainWrapper.classList.remove(styles.columnFlex);
+	}, []);
 	return (
 		<Layout>
-			<main className={styles.mainContainer}>New content coming soon!</main>
+			<main className={styles.mainContainer}>
+				There is nothing here at the moment, meanwhile checkout:
+				<br />
+				<br />
+				<ul>
+					<li>
+						<Link to="/blog/2020/08/21/react-opentable">
+							🎉 Checkout <code>react-opentable</code> 🎉
+						</Link>
+					</li>
+					<li>
+						<Link to="/blog/2020/08/16/Soft-skills">Review of Soft Skills</Link>
+					</li>
+					<li>
+						<Link to="/books">Books that I'm reading right now</Link>
+					</li>
+				</ul>
+			</main>
 		</Layout>
 	);
 }
