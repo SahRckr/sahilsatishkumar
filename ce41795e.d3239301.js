f34368f27a64909daba9b7941a477c3f5987de73
Sahil Satishkumar <sahilsatishkumar@gmail.com>
2020-08-21 22:10:10 +0530

    Updates

---
+++ ce41795e.d3239301.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[22],{80:function(a){a.exports=JSON.parse('{"allTagsPath":"/blog/tags","slug":"react-opentable","name":"react-opentable","count":1,"permalink":"/blog/tags/react-opentable"}')}}]);
