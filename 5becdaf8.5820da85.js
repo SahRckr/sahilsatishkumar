fa395dac2e6ffc0b90e72f8881ba18df2712896a
Sahil Satishkumar <sahilsatishkumar@gmail.com>
2020-08-22 15:03:32 +0530

    Updates

---
+++ 5becdaf8.5820da85.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{64:function(a){a.exports=JSON.parse('{"allTagsPath":"/blog/tags","slug":"opentable","name":"opentable","count":1,"permalink":"/blog/tags/opentable"}')}}]);
